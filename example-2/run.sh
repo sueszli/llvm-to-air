4db03635d4e1a5ddf81b0d3eb920ef44522604a4
sueszli <jabaryyahya@gmail.com>
2025-12-28 06:53:14 +0100

    add example-2 with ll step

---
+++ example-2/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "compiling: MSL -> LLVM IR"
+xcrun -sdk macosx metal -S -emit-llvm shader.metal -o shader.ll
+
+echo "compiling: LLVM IR -> AIR"
+xcrun -sdk macosx metal -c shader.ll -o shader.air
+
+echo "compiling: AIR -> metallib"
+xcrun -sdk macosx metallib shader.air -o shader.metallib
+
+echo "running harness"
+clang++ -std=c++17 -framework Metal -framework Foundation harness.mm -o harness
+./harness
+rm harness shader.air shader.metallib shader.ll
