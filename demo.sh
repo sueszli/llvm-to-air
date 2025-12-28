134945c02b94bafb1b0e47813eacaef2450746ed
sueszli <jabaryyahya@gmail.com>
2025-12-28 09:01:52 +0100

    up

--- demo.sh
+++ demo.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "forging AIR..."
 python3 air_forge.py input.ll > test.ll
 xcrun -sdk macosx metal -c test.ll -o test.air
 xcrun -sdk macosx metallib test.air -o test.metallib
-echo "compiling harness..."
 clang++ -std=c++17 -framework Metal -framework Foundation verify.mm -o verify
-echo "running verification..."
 ./verify
 rm test.ll test.air test.metallib verify
