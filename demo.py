aa61071f63163904aefb536f8dcd833107da0e83
sueszli <jabaryyahya@gmail.com>
2025-12-29 22:17:14 +0100

    up

--- demo.py
+++ demo.py
@@ -22,7 +22,6 @@ from xdsl.dialects import arith, func, llvm, scf
 from xdsl.dialects.builtin import Block, FloatAttr, FunctionType, IntegerAttr, ModuleOp, f32, i32, i64
 from xdsl.printer import Printer
 
-# add src and test dirs to path to import utils and llvm_to_air
 root_dir = Path(__file__).resolve().parent
 from src.air_to_metallib import compile_to_metallib, create_compute_pipeline, execute_kernel
 from src.llvm_to_air import to_air
